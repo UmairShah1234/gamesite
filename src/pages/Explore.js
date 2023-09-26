128561d2da300e1a342da51363446228ec37dbab
unknown <taabishshaikh2>
2023-09-26 23:29:57 +0530

    new branch explore with cahnges in explore page

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,108 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React from "react";
+import { products } from "../products";
 
 const Explore = () => {
+  const theme = useTheme();
+  const colors = theme.palette.mode;
   return (
-    <div>Explore</div>
-  )
-}
+    <>
+      {products.map((product) => {
+        return (
+          <div className="flex items-center justify-center">
+            {product.id % 2 === 0 ? (
+              <Card
+                key={product.id}
+                className="m-2 overflow-hidden flex items-center justify-center  "
+                elevation={20}
+                sx={{ width: "70%" }}
+              >
+                <CardMedia
+                  component={"img"}
+                  // hover:w-1/2  w-3/4
+                  sx={{ width: 300 }}
+                  // className="h-80 "
+                  src={product.img}
+                />
+                <CardContent>
+                  <Typography
+                    className="flex items-center justify-center"
+                    component="div"
+                    variant="h5"
+                  >
+                    {product.title}
+                  </Typography>
+                  <Typography
+                    className="  flex items-center justify-center  "
+                    variant="h6"
+                    component="div"
+                  >
+                    {product.desc}
+                  </Typography>
+                  <button
+                      className={`border hover:bg-${
+                        colors === "dark" ? "stone-600" : "stone-300"
+                      } p-3 border-${colors === "dark" ? "white" : "black"}`}
+                      variant="outlined"
+                    >
+                      Rent VR
+                    </button>
+                  </CardContent>
+                 
+                    
+                
+              </Card>
+            ) : (
+              <Card
+                key={product.id}
+                className="m-2 overflow-hidden flex items-center justify-center  "
+                elevation={20}
+                sx={{ width: "70%" }}
+              >
+                <CardContent>
+                  <Typography
+                    className="flex items-center justify-center"
+                    component="div"
+                    variant="h5"
+                  >
+                    {product.title}
+                  </Typography>
+                  <Typography
+                    className="  flex items-center justify-center  "
+                    variant="h6"
+                    component="div"
+                  >
+                    {product.desc}
+                  </Typography>
+                  <button
+                      className={`border hover:bg-${
+                        colors === "dark" ? "stone-600" : "stone-300"
+                      } p-3 border-${colors === "dark" ? "white" : "black"}`}
+                      variant="outlined"
+                    >
+                      Rent VR
+                    </button>
+                </CardContent>
+                <CardMedia
+                  component={"img"}
+                  // hover:w-1/2  w-3/4
+                  sx={{ width: 300 }}
+                  // className="h-80 "
+                  src={product.img}
+                />
+              </Card>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-export default Explore
+export default Explore;
---
+++ src/products.js
@@ -0,0 +1,30 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1622979135225-d2ba269cf1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "Prouct1",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
+      
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1622979135225-d2ba269cf1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "Prouct2",
+      desc:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
+      
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1622979135225-d2ba269cf1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "Prouct3",
+      desc:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
+   
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1622979135225-d2ba269cf1ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+      title: "Prouct4",
+      desc:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditatenihil illo incidunt officiis iusto! Excepturi vero nisi molestiae doloribus illum repudiandae, dolorem omnis, numquam reprehenderiteaque sed commodi voluptatem sit!",
+   
+    },
+  ];
